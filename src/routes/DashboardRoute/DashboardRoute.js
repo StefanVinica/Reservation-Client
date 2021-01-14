36fb3c6783e95ebe254639e1d855160e32182172
StefanVinica <stefanikolovski996@gmail.com>
2021-01-13 16:16:43 -0800

    inital commit with login and sign up

---
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class DashboardRoute extends Component {
+  render() {
+    return (
+      <section>
+        Sucsess
+      </section>
+    );
+  }
+}
+
